380e8c89c0ab90aa895a96d6bc866a9bf6d2fce2
momchilmitev <momchilmitev87@gmail.com>
2020-12-14 19:03:52 +0100

    added implementation of the search form with attaching the events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,31 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Video search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
