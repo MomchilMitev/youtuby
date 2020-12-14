facb245a2b8e6846ae55e4aff846c1519d2104a9
momchilmitev <momchilmitev87@gmail.com>
2020-12-14 20:49:13 +0100

    added logic for fetching videos

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
 import React from "react";
 
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
+
+const KEY = "AIzaSyArj6mVcDmeGxzlo0xNEqIgNGn8sUplWrU";
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onFormSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        type: "video",
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onFormSubmit} />
+        {this.state.videos.length}
       </div>
     );
   }
