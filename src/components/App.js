8b5d878248ed986e9517308ef90ceaad154feceb
momchilmitev <momchilmitev87@gmail.com>
2020-12-15 08:03:37 +0100

    added video detail component to display video player and default search

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from "react";
 
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetails from "./VideoDetails";
 import youtube from "../apis/youtube";
 
 const KEY = "AIzaSyArj6mVcDmeGxzlo0xNEqIgNGn8sUplWrU";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onFormSubmit("acura");
+  }
 
   onFormSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -20,14 +25,33 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onFormSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetails video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
